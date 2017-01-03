e4ca5941081c72d77ef13595a7754461d7e30656
Flavio Calva <flavio@ya2.it>
2017-01-03 22:59:01 +0100

    rotating the weapon

--- racing/weapon/rocket/gfx.py
+++ racing/weapon/rocket/gfx.py
@@ -2,6 +2,7 @@ from os.path import exists
 from yyagl.gameobject import Gfx
 from panda3d.bullet import BulletRigidBodyNode, BulletBoxShape, BulletSphereShape
 from panda3d.core import Mat4
+from direct.interval.LerpInterval import LerpHprInterval
 
 
 class RocketGfx(Gfx):
@@ -17,8 +18,12 @@ class RocketGfx(Gfx):
         self.gfx_np.set_h(180)
         self.gfx_np.set_scale(1.5)
         self.gfx_np.setPos(0, 0, 1.5)
+        self.ival = LerpHprInterval(self.gfx_np, 3, (180, 0, 360), (180, 0, 0))
+        self.ival.loop()
 
     def destroy(self):
         self.gfx_np.remove_node()
         self.gfx_np = None
+        self.ival.finish()
+        self.ival = None
         Gfx.destroy(self)
