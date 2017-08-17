5078eaa1d641afec7f1a8d5440a3de90383f8a77
Flavio Calva <flavio@ya2.it>
2017-08-17 20:15:51 +0200

    fixed loading's background

--- racing/race/gui/loading/menu.py
+++ racing/race/gui/loading/menu.py
@@ -1,3 +1,5 @@
+from random import randint
+from copy import deepcopy
 from yyagl.gameobject import GameObject, Gui
 from yyagl.engine.gui.menu import Menu, MenuLogic
 from .loadingpage import LoadingPage
@@ -8,7 +10,11 @@ class LoadingGui(Gui):
     def __init__(self, mdt, rprops, sprops, loading, track_name_transl,
                  drivers):
         Gui.__init__(self, mdt)
-        self.menu = Menu(rprops.menu_args)
+        pbackground = 'assets/images/loading/%s%s.jpg'
+        pbackground = pbackground % (rprops.track_name, randint(1, 4))
+        menu_args = deepcopy(rprops.menu_args)
+        menu_args.background_img = pbackground
+        self.menu = Menu(menu_args)
         self.menu.loading = loading
         self.menu.push_page(LoadingPage(
             rprops, sprops, self.menu, track_name_transl, drivers))
