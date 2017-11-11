27634491a2b5d0ef17cf33f0e8c065296f2ea78e
Flavio Calva <flavio@ya2.it>
2017-11-11 18:44:53 +0100

    refactoring of translations (issue #12)

--- build/strings.py
+++ build/strings.py
@@ -4,39 +4,42 @@ from shutil import move, copy
 from .build import files
 
 
-def bld_strings(target, source, env):
-    map(lambda lng: __bld_strings_lng(lng, env), env['LANGUAGES'])
+def bld_mo(target, source, env):
+    lng_dir_code, appname = env['LNG'], env['APPNAME']
+    lng_code = str(target[0])[len(lng_dir_code):].split('/')[0]
+    map(lambda lng: __bld_mo(lng, lng, env), env['LANGUAGES'])
 
 
-def __bld_strings_lng(lng, env):
+def __bld_mo(lng, lng_code, env):
     lng_dir = env['LNG'] + lng + '/LC_MESSAGES/'
-    cmd = 'msgfmt -o {lng_dir}{appname}.mo {lng_dir}{appname}.po'
-    system(cmd.format(lng_dir=lng_dir, appname=env['APPNAME']))
+    cmd = 'msgfmt -o {lng_dir}{appname}.mo assets/po/{lng_code}.po'
+    system(cmd.format(lng_dir=lng_dir, appname=env['APPNAME'], lng_code=lng_code))
 
 
-def bld_tmpl_merge(target, source, env):
+def bld_pot(target, source, env):
     src_files = ' '.join(files(['py'], ['feedparser', 'venv']))
-    lng_dir, appname, langs = env['LNG'], env['APPNAME'], env['LANGUAGES']
-    cmd_tmpl = 'xgettext -d {appname} -L python -o {appname}.pot '
+    lng_dir, appname = env['LNG'], env['APPNAME']
+    cmd_tmpl = 'xgettext -d {appname} -L python -o assets/po/{appname}.pot '
     system(cmd_tmpl.format(lng_dir=lng_dir, appname=env['APPNAME']) + src_files)
-    lng_code = str(target[0])[len(lng_dir):].split('/')[0]
-    __bld_tmpl_merge(lng_dir, lng_code, appname)
-    remove(appname + '.pot')
 
 
-def __bld_tmpl_merge(lng_dir, lng, appname):
+def bld_merge(target, source, env):
+    lng_dir, appname = env['LNG'], env['APPNAME']
+    lng_code = str(target[0])[len('assets/po/'):].split('.po')[0]
+    __bld_tmpl_merge(lng_dir, lng_code, lng_code, appname)
+
+
+def __bld_tmpl_merge(lng_dir, lng_code, lng, appname):
     lng_base_dir = __prepare(lng_dir, lng, appname)
-    __merge(lng_base_dir, appname)
-    __postprocess(lng_base_dir, appname)
+    __merge(lng_base_dir, lng_code, appname)
+    __postprocess(lng_base_dir, lng_code, appname)
 
 
 def __prepare(lng_base_dir, lng, appname):
     if not exists(lng_base_dir + lng + '/LC_MESSAGES'):
         makedirs(lng_base_dir + lng + '/LC_MESSAGES')
-    dst = lng_base_dir + lng + '/LC_MESSAGES/%s.pot' % appname
-    copy(appname + '.pot', dst)
     lng_dir = lng_base_dir + lng + '/LC_MESSAGES/'
-    if not exists(lng_dir + appname + '.po'):
+    if not exists('assets/po/' + lng + '.po'):
         lines_to_fix = ['CHARSET/UTF-8', 'ENCODING/8bit']
         map(lambda line: __fix_line(line, lng_dir, appname), lines_to_fix)
         copy(lng_dir + appname + '.pot', lng_dir + appname + '.po')
@@ -50,16 +53,16 @@ def __fix_line(line, lng_dir, appname):
     move(lng_dir + appname + 'tmp.po', lng_dir + appname + '.pot')
 
 
-def __merge(lng_dir, appname):
+def __merge(lng_dir, lng_code, appname):
     cmd_str = 'msgmerge -o {lng_dir}{appname}merge.po ' + \
-        '{lng_dir}{appname}.po {lng_dir}{appname}.pot'
-    system(cmd_str.format(lng_dir=lng_dir, appname=appname))
-    copy(lng_dir + appname + 'merge.po', lng_dir + appname + '.po')
+        'assets/po/{lng_code}.po assets/po/{appname}.pot'
+    system(cmd_str.format(lng_dir=lng_dir, lng_code=lng_code, appname=appname))
+    copy(lng_dir + appname + 'merge.po', 'assets/po/%s.po' % lng_code)
+    remove('{lng_dir}{appname}merge.po'.format(lng_dir=lng_dir, appname=appname))
 
-
-def __postprocess(lng_dir, appname):
-    lines = open(lng_dir + appname + '.po', 'r').readlines()
-    with open(lng_dir + appname + '.po', 'w') as outf:
+def __postprocess(lng_dir, lng_code, appname):
+    lines = open('assets/po/%s.po' % lng_code, 'r').readlines()
+    with open('assets/po/%s.po' % lng_code, 'w') as outf:
         for line in lines:
             po_str = '"POT-Creation-Date: YEAR-MO-DA HO:MI+ZONE\\n"\n'
             outf.write(po_str if line.startswith(po_str[:20]) else line)
