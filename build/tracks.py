d10a897cf801ed1df508830752f39733599059aa
Flavio Calva <flavio@ya2.it>
2018-01-02 16:08:21 +0100

    using psutil

--- build/tracks.py
+++ build/tracks.py
@@ -3,6 +3,7 @@ from .mtprocesser import MultithreadedProcesser
 
 
 def bld_models(target, source, env):
+    system('pip install psutil')
     mp_mgr = MultithreadedProcesser(env['CORES'])
     for root, dnames, fnames in walk(env['MODELS_DIR_PATH']):
         if not root.startswith(env['TRACKS_DIR_PATH']):
