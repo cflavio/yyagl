e42744df97cf9b9003d6ead656d85fcee62d0f9a
Flavio Calva <flavio@ya2.it>
2018-01-25 21:45:32 +0100

    muc

--- engine/network/xmpp.py
+++ engine/network/xmpp.py
@@ -67,6 +67,7 @@ class XMPP(Subject):
         self.xmpp.register_plugin('xep_0004') # Data Forms
         self.xmpp.register_plugin('xep_0060') # PubSub
         self.xmpp.register_plugin('xep_0199') # XMPP Ping
+        self.xmpp.register_plugin('xep_0045') # Multi-User Chat
         #self.xmpp.auto_authorize = False
         #self.xmpp.auto_subscribe = False
         if self.xmpp.connect(): self.xmpp.process()
@@ -103,6 +104,7 @@ class YorgClient(ClientXMPP):
         self.add_event_handler('session_start', lambda msg: self.dispatch_msg('session_start', msg))
         self.add_event_handler('failed_auth', lambda msg: self.dispatch_msg('failed_auth', msg))
         self.add_event_handler('message', lambda msg: self.dispatch_msg('message', msg))
+        self.add_event_handler('groupchat_message', lambda msg: self.dispatch_msg('groupchat_message', msg))
         self.add_event_handler('presence_subscribe', lambda msg: self.dispatch_msg('subscribe', msg))
         self.add_event_handler('presence_subscribed', lambda msg: self.dispatch_msg('subscribed', msg))
         self.add_event_handler('presence_available', lambda msg: self.dispatch_msg('presence_available', msg))
@@ -112,6 +114,7 @@ class YorgClient(ClientXMPP):
         if code == 'session_start': self.cb_mux.add_cb(self.session_start, [msg])
         if code == 'failed_auth': self.cb_mux.add_cb(self.on_ko, [msg])
         if code == 'message': self.cb_mux.add_cb(self.on_message, [msg])
+        if code == 'groupchat_message': self.cb_mux.add_cb(self.on_groupchat_message, [msg])
         if code == 'subscribe': self.cb_mux.add_cb(self.on_subscribe, [msg])
         if code == 'subscribed': self.cb_mux.add_cb(self.on_subscribed, [msg])
         if code == 'presence_available': self.cb_mux.add_cb(self.on_presence_available, [msg])
@@ -122,6 +125,10 @@ class YorgClient(ClientXMPP):
         self.get_roster()
         logging.info(self.client_roster)
         self.on_ok()
+        res = self['xep_0030'].get_items(jid=self.xmpp.xmpp.boundjid.server, iterator=True)
+        for itm in res['disco_items']:
+            if 'conference' in itm['jid']:
+                self.xmpp.xmpp.conf_srv = itm['jid']
 
     @property
     def friends(self):
@@ -138,7 +145,9 @@ class YorgClient(ClientXMPP):
 
     def on_presence_available(self, msg):
         if str(msg['from']) not in [usr.name_full for usr in self.xmpp.users]:
-            self.xmpp.users += [User(msg['from'], 0, False, self.xmpp)]
+            self.xmpp.users += [User(msg['from'], 0, True, self.xmpp)]
+            # TODO: create with is_in_yorg == False and use a stanza for
+            # setting is_in_yorg = True
         self.sort_users()
         if msg['from'].bare != self.xmpp.xmpp.boundjid.bare and msg['from'] not in self.presences_sent:
             self.presences_sent += [msg['from']]
@@ -160,6 +169,12 @@ class YorgClient(ClientXMPP):
             return self.on_answer_full(msg)
         if msg['subject'] == 'chat':
             return self.xmpp.notify('on_msg', msg)
+        if msg['subject'] == 'invite':
+            return self.xmpp.notify('on_invite_chat', msg)
+
+    def on_groupchat_message(self, msg):
+        if msg['mucnick'] != self.xmpp.xmpp.boundjid.bare:
+            return self.xmpp.notify('on_groupchat_msg', msg)
 
     def on_list_users(self, msg):
         self.xmpp.users = []
@@ -172,9 +187,10 @@ class YorgClient(ClientXMPP):
         filter_names = ['ya2_yorg@jabb3r.org', self.boundjid.bare]
         presence_users = [usr.name_full for usr in self.xmpp.users if usr.name not in filter_names]
         for usr in presence_users:
-            self.xmpp.xmpp.send_presence(
-                pfrom=self.xmpp.xmpp.boundjid.full,
-                pto=usr)
+            if usr not in self.presences_sent:
+                self.xmpp.xmpp.send_presence(
+                    pfrom=self.xmpp.xmpp.boundjid.full,
+                    pto=usr)
 
         self.sort_users()
         self.xmpp.notify('on_users')
