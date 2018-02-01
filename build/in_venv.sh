c8c04cb608a60e304b7d3ae7d16450fed6b9f647
Flavio Calva <flavio@ya2.it>
2018-02-01 19:29:40 +0100

    reinstalling setuptools

--- build/in_venv.sh
+++ build/in_venv.sh
@@ -3,5 +3,6 @@
 virtualenv venv
 . ./venv/bin/activate
 pip install --upgrade pip
+pip install --upgrade setuptools
 pip install panda3d -i https://archive.panda3d.org/branches/deploy-ng --upgrade
 scons "$@"
