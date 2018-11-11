c58bebd72e22ba66bcbe52cc3916dd1e8763df4a
Flavio Calva <flavio@ya2.it>
2018-11-11 12:53:13 +0100

    sfx

--- racing/weapon/mine/mine.py
+++ racing/weapon/mine/mine.py
@@ -1,4 +1,4 @@
-from ..weapon.weapon import PhysWeapon
+from ..weapon.weapon import PhysWeapon, WeaponAudio
 from ..weapon.gfx import WeaponGfxNetwork
 from .phys import MinePhys
 from .logic import MineLogic, MineLogicNetwork
@@ -9,10 +9,15 @@ from .ai import MineAi
 class MineGfxNetwork(WeaponGfxNetwork): pass
 
 
+class MineAudio(WeaponAudio):
+    sfx = 'assets/sfx/mine.ogg'
+
+
 class Mine(PhysWeapon):
     phys_cls = MinePhys
     logic_cls = MineLogic
     event_cls = MineEvent
+    audio_cls = MineAudio
     ai_cls = MineAi
 
 
