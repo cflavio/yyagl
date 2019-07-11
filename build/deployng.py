64811d0f1fe6fa31e45318fcfdf5f5209a37a466
Flavio Calva <flavio@ya2.it>
2019-07-11 19:09:42 +0200

    building with panda3d 1.10.3

--- build/deployng.py
+++ build/deployng.py
@@ -9,7 +9,7 @@ bson
 pyyaml
 feedparser'''
 requirements = '''
-panda3d==1.10.2'''
+panda3d==1.10.3'''
 excl_patterns = ['build/*', 'built/*', 'setup.py', 'requirements.txt', '*.swp',
                  'SConstruct', 'venv/*', '.git*', '*.pyc', 'options.yml']
 incl_patterns = ['*.txo', '*.txt', '*.ttf', '*.bam', '*.vert', '*.frag', '*.yml']
