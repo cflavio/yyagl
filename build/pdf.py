a3454495d56670f91c418308e9f21849d162709b
Flavio Calva <flavio@ya2.it>
2019-11-24 20:01:42 +0100

    refactoring for tests

--- build/pdf.py
+++ build/pdf.py
@@ -1,7 +1,8 @@
 from os import system, rename, remove
+from os.path import exists, dirname, basename
 from itertools import product
 from shutil import move
-from .build import bld_dpath, branch, pdf_fpath
+from .build import bld_dpath, branch, pdf_fpath, exec_cmd
 
 
 def bld_pdfs(target, source, env):
@@ -20,10 +21,18 @@ def __bld_pdf(name, opts):
         "--continuous-page-numbers --no-header {lng} -o - tmp.txt | " + \
         "ps2pdf - {name}.pdf ; rm tmp.txt"
     # command for generating a pdf with appended files
-    for (i, opt), suff in product(enumerate(opts), ['', '_cont']):
-        __process(opt, cont_tmpl if suff else tmpl, name + suff, i)
+    test_tmpl = "tail -n +1 {found} " + \
+        "| sed 's/==> /# ==> /' > tmp.txt ; enscript --font=Courier10 " + \
+        "--continuous-page-numbers --no-header {lng} -o - tmp.txt | " + \
+        "ps2pdf - {name}.pdf ; rm tmp.txt"
+    # command for generating a pdf with appended files
+    for (i, opt), suff in product(enumerate(opts), ['', '_cont', '_test']):
+        if suff == '': curr_tmpl = tmpl
+        elif suff == '_cont': curr_tmpl = cont_tmpl
+        else: curr_tmpl = test_tmpl
+        __process(opt, curr_tmpl, name + suff, i)
     pdf_tmpl = 'pdfnup --nup 2x1 -o {name}.pdf {name}.pdf'
-    suffixes = [name + '', name + '_cont']
+    suffixes = [name + '', name + '_cont', name + '_test']
     list(map(lambda name_suff: system(pdf_tmpl.format(name=name_suff)), suffixes))
 
 
@@ -32,10 +41,25 @@ def __process(opt, tmpl, name, i):
     lng = ('--pretty-print=' + opt.lng) if opt.lng else ''
     wcard = '-o '.join(["-name '%s' " % wld for wld in opt.fil.split()])
     wcard = '\\( %s\\)' % wcard
+    found = __myfind(opt.root, wcard, filt)
     cmd = tmpl.format(lng=lng, root=opt.root, wildcard=wcard, filter=filt,
-                      name=name)
+                      name=name, found=found)
     __process_step(name, cmd) if i else system(cmd)
 
+def __myfind(root, wildcard, filter):
+    tmpl = 'find {root} {wildcard} {filter}'
+    cmd = tmpl.format(root=root, wildcard=wildcard, filter=filter)
+    found_files = exec_cmd(cmd)
+    found_files = found_files.split('\n')
+    ret = []
+    for _file in found_files:
+        ret += [_file]
+        dirn = dirname(_file)
+        basen = basename(_file)
+        if not _file.startswith('./yyagl'): continue
+        testn = './yyagl/tests/' + dirn[2:] + '/test_' + basen
+        if exists(testn): ret += [testn]
+    return ' '.join(ret)
 
 def __process_step(name, cmd):
     rename(name + '.pdf', name + '_append.pdf')
@@ -50,6 +74,7 @@ def __process_step(name, cmd):
 def __bld_pkg(env):
     pdfs = ''.join([name + '.pdf ' for name in env['PDF_CONF']])
     pdfs += ''.join([name + '_cont.pdf ' for name in env['PDF_CONF']])
+    pdfs += ''.join([name + '_test.pdf ' for name in env['PDF_CONF']])
     cmd = 'tar -czf {fout} ' + pdfs + ' && rm ' + pdfs
     pdf_path = pdf_fpath.format(dst_dir=bld_dpath, appname=env['APPNAME'],
                                 version=branch)
--- decorator.py
+++ decorator.py
@@ -1,9 +1,9 @@
-class Decorator:
+# class Decorator:
 
-    def __init__(self, decorated):
-        self.__dict__['_decorated'] = decorated
+#     def __init__(self, decorated):
+#         self.__dict__['_decorated'] = decorated
 
-    def __getattr__(self, attr): return getattr(self._decorated, attr)
+#     def __getattr__(self, attr): return getattr(self._decorated, attr)
 
-    def __setattr__(self, attr, value):
-        return setattr(self._decorated, attr, value)
+#     def __setattr__(self, attr, value):
+#         return setattr(self._decorated, attr, value)
