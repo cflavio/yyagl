09f4768a99009c6b993b49eec382da743fd0877d
Flavio Calva <flavio@ya2.it>
2020-02-27 19:04:01 +0100

    setuptools: dev info

--- setup.py
+++ setup.py
@@ -1,29 +1,43 @@
 from distutils.cmd import Command
-from distutils.log import INFO
 from setuptools import setup
+from build.build import files
 from build.src import bld_src
+from build.devinfo import bld_devinfo
 
 
-class SourcePkgCommand(Command):
+class AbsCmd(Command):
 
+    env = {'APPNAME': 'yyagl'}
     user_options = []
 
     def initialize_options(self): pass
 
     def finalize_options(self): pass
 
+
+class SourcePkgCmd(AbsCmd):
+
+    def run(self): bld_src(None, None, AbsCmd.env)
+
+
+class DevInfoCmd(AbsCmd):
+
     def run(self):
-        self.announce(
-            'Building source package',
-            level=INFO)
-        bld_src(None, None, {'APPNAME': 'yyagl'})
+        def cond_yyagl(src):
+            thirdparty = str(src).startswith('yyagl/thirdparty/')
+            venv = str(src).startswith('venv/')
+            racing = str(src).startswith('yyagl/racing/')
+            return thirdparty or venv or racing or \
+                str(src).startswith('yyagl/tests')
+        dev_conf = {'devinfo_yyagl': cond_yyagl}
+        AbsCmd.env['DEV_CONF'] = dev_conf
+        bld_devinfo(None, files(['py']), AbsCmd.env)
 
 
 if __name__ == '__main__':
     setup(
         cmdclass={
-            'source_pkg': SourcePkgCommand,
-        },
+            'source_pkg': SourcePkgCmd,
+            'devinfo': DevInfoCmd},
         name='Yyagl',
-        version=0.12
-    )
+        version=0.12)
