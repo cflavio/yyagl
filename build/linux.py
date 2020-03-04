aaa9bf340b3ed30a0f0b2a76f2c135f7182f53bb
Flavio Calva <flavio@ya2.it>
2020-03-04 23:12:21 +0100

    linux and windows builds

--- build/linux.py
+++ build/linux.py
@@ -1,4 +1,4 @@
-from os import remove, system, makedirs, walk, chdir
+from os import remove, system, makedirs, walk, chdir, getcwd
 from os.path import basename, dirname, realpath, exists, abspath
 from shutil import move, rmtree, copytree, copy
 from glob import glob
@@ -9,16 +9,14 @@ from yyagl.build.deployng import bld_ng
 
 def bld_linux(target, source, env):
     ico_fpath = env['ICO_FPATH']
-    bld_ng(env['APPNAME'], linux=True)
-
-    src = '{dst_dir}../dist/{appname}-0.0.0_manylinux1_x86_64.tar.xz'
+    chdir('..')  # after the previous command we are in 'dist'
+    src = '{dst_dir}../dist/{appname}-{version}_manylinux1_x86_64.tar.xz'
     tgt_file = '{dst_dir}{appname}-{version}-linux_amd64.tar.xz'
     src_fmt = src.format(dst_dir=bld_dpath, appname=env['APPNAME'],
                          version=branch)
     tgt_fmt = tgt_file.format(dst_dir=bld_dpath, appname=env['APPNAME'],
                               version=branch)
     move(src_fmt, tgt_fmt)
-
     start_dir = abspath('.') + '/'
     if exists(bld_dpath + 'linux'): rmtree(bld_dpath + 'linux')
     makedirs(bld_dpath + 'linux')
@@ -33,8 +31,8 @@ def _do_bld(start_dir, appname, ico_fpath, clean=True):
     _bld_pckgs(appname)
     chdir('../..')
     if not clean: return
-    rmtree('build')
-    rmtree('dist')
+    #rmtree('build')
+    #rmtree('dist')
 
 
 def _prepare(start_path):
--- build/windows.py
+++ build/windows.py
@@ -7,22 +7,20 @@ from yyagl.build.deployng import bld_ng
 
 
 def bld_windows(target, source, env):
-    bld_ng(env['APPNAME'], win=True)
-
-    src = '{dst_dir}../dist/{appname}-0.0.0_win_amd64.tar.xz'
+    src = '{dst_dir}../dist/{appname}-{version}_win_amd64.tar.xz'
     tgt_file = '{dst_dir}{appname}-{version}-windows.tar.xz'
     src_fmt = src.format(dst_dir=bld_dpath, appname=env['APPNAME'],
                          version=branch)
     tgt_fmt = tgt_file.format(dst_dir=bld_dpath, appname=env['APPNAME'],
                               version=branch)
-    move(src_fmt, tgt_fmt)
+    if exists(src_fmt): move(src_fmt, tgt_fmt)
 
-    src = '{dst_dir}../build/{appname}-0.0.0.exe'
+    src = '{dst_dir}../build/{appname}-{version}.exe'
     tgt_file = '{dst_dir}{appname}-{version}-windows.exe'
     src_fmt = src.format(dst_dir=bld_dpath, appname=env['APPNAME'],
                          version=branch)
     tgt_fmt = tgt_file.format(dst_dir=bld_dpath, appname=env['APPNAME'],
                               version=branch)
-    move(src_fmt, tgt_fmt)
-    rmtree('build')
-    rmtree('dist')
+    if exists(src_fmt): move(src_fmt, tgt_fmt)
+    #rmtree('build')
+    #rmtree('dist')
