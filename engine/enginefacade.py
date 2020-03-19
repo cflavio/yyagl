168af6cb4c83c32d320c60c5d8929e5be3c624b3
Flavio Calva <flavio@ya2.it>
2020-03-19 19:04:08 +0100

    refactoring of engine's facade

--- engine/enginefacade.py
+++ engine/enginefacade.py
@@ -1,51 +1,74 @@
+from math import pi
 from yyagl.facade import Facade
 
 
 class EngineFacade(Facade):
 
-    def __init__(self):
-        # lambdas are for lazy evaluation (they may not exist)
-        prop_lst = [
-            ('version', lambda obj: obj.logic.version),
-            ('curr_path', lambda obj: obj.logic.curr_path),
-            ('cfg', lambda obj: obj.logic.cfg),
-            ('is_runtime', lambda obj: obj.logic.is_runtime),
-            ('languages', lambda obj: obj.logic.cfg.lang_cfg.languages),
-            ('resolutions', lambda obj: obj.gui.resolutions),
-            ('closest_resolution', lambda obj: obj.gui.closest_resolution),
-            ('joystick_mgr', lambda obj: obj.event.joystick_mgr),
-            ('curr_time', lambda obj: self.clock.time)]
-        mth_lst = [
-            ('attach_obs', lambda obj: obj.event.attach),
-            ('detach_obs', lambda obj: obj.event.detach),
-            ('attach_node', lambda obj: obj.gfx.root.attach_node),
-            ('particle', lambda obj: obj.gfx.particle),
-            ('init_gfx', lambda obj: obj.gfx.init),
-            ('clean_gfx', lambda obj: obj.gfx.clean),
-            ('set_cam_pos', lambda obj: base.camera.set_pos),
-            ('load_font', lambda obj: self.eng.font_mgr.load_font),
-            ('open_browser', lambda obj: obj.gui.open_browser),
-            ('toggle_pause', lambda obj: obj.pause.logic.toggle),
-            ('play', lambda obj: obj.audio.play),
-            ('set_volume', lambda obj: obj.audio.set_volume),
-            ('show_cursor', lambda obj: obj.gui.cursor.show),
-            ('show_standard_cursor', lambda obj: obj.gui.cursor.show_standard),
-            ('hide_cursor', lambda obj: obj.gui.cursor.hide),
-            ('hide_standard_cursor', lambda obj: obj.gui.cursor.hide_standard),
-            ('cursor_top', lambda obj: obj.gui.cursor.cursor_top),
-            ('set_amb_lgt', lambda obj: obj.shader_mgr.set_amb_lgt),
-            ('set_dir_lgt', lambda obj: obj.shader_mgr.set_dir_lgt),
-            ('clear_lights', lambda obj: obj.shader_mgr.clear_lights),
-            ('toggle_shader', lambda obj: obj.shader_mgr.toggle_shader),
-            ('set_resolution', lambda obj: obj.gui.set_resolution),
-            ('toggle_fullscreen', lambda obj: obj.gui.toggle_fullscreen),
-            ('send', lambda obj: obj.lib.send),
-            ('do_later', lambda obj: obj.lib.do_later),
-            ('add_task', lambda obj: obj.lib.add_task),
-            ('remove_task', lambda obj: obj.lib.remove_task),
-            ('log', lambda obj: obj.log_mgr.log),
-            ('log_tasks', lambda obj: obj.log_mgr.log_tasks)]
-        Facade.__init__(self, prop_lst, mth_lst)
+    @property
+    def version(self): return self.logic.version
+
+    @property
+    def curr_path(self): return self.logic.curr_path
+
+    @property
+    def cfg(self): return self.logic.cfg
+
+    @property
+    def is_runtime(self): return self.logic.is_runtime
+
+    @property
+    def languages(self): return self.logic.cfg.lang_cfg.languages
+
+    @property
+    def resolutions(self): return self.gui.resolutions
+
+    @property
+    def closest_resolution(self): return self.gui.closest_resolution
+
+    @property
+    def joystick_mgr(self): return self.event.joystick_mgr
+
+    @property
+    def curr_time(self): return self.clock.time
+
+    def attach_obs(self, obs_meth, sort=10, rename='', args=[]):
+        return self.event.attach(obs_meth, sort, rename, args)
+    def detach_obs(self, obs_meth, lambda_call=None):
+        return self.event.detach(obs_meth, lambda_call)
+    def attach_node(self, name): return self.gfx.root.attach_node(name)
+    def particle(
+            self, parent, texture, color=(1, 1, 1, 1), ampl=pi/6, ray=.5,
+            rate=.0001, gravity=-.85, vel=3.8, part_duration=1.0,
+            autodestroy=None):
+        return self.gfx.particle(
+            parent, texture, color, ampl, ray, rate, gravity, vel,
+            part_duration, autodestroy)
+    def init_gfx(self): return self.gfx.init()
+    def clean_gfx(self): return self.gfx.clean()
+    def set_cam_pos(self, pos): return base.camera.set_pos(pos)
+    def load_font(self, fpath, outline=True):
+        return self.eng.font_mgr.load_font(fpath, outline)
+    def open_browser(self, url): return self.gui.open_browser(url)
+    def toggle_pause(self, show_frm=True): return self.pause.logic.toggle(show_frm)
+    def play(self): return self.audio.play()
+    def set_volume(self, vol): return self.audio.set_volume(vol)
+    def show_cursor(self): return self.gui.cursor.show()
+    def show_standard_cursor(self): return self.gui.cursor.show_standard()
+    def hide_cursor(self): return self.gui.cursor.hide()
+    def hide_standard_cursor(self): return self.gui.cursor.hide_standard()
+    def cursor_top(self): return self.gui.cursor.cursor_top()
+    def set_amb_lgt(self, col): return self.shader_mgr.set_amb_lgt(col)
+    def set_dir_lgt(self, col, direction): return self.shader_mgr.set_dir_lgt(col, direction)
+    def clear_lights(self): return self.shader_mgr.clear_lights()
+    def toggle_shader(self): return self.shader_mgr.toggle_shader()
+    def set_resolution(self, res, fullscreen=None): return self.gui.set_resolution(res, fullscreen)
+    def toggle_fullscreen(self): return self.gui.toggle_fullscreen()
+    def send(self, msg): return self.lib.send(msg)
+    def do_later(self, time, meth, args=None): return self.lib.do_later(time, meth, args)
+    def add_task(self, mth, priority=0): return self.lib.add_task(mth, priority)
+    def remove_task(self, tsk): return self.lib.remove_task(tsk)
+    def log(self, msg, verbose=False): return self.log_mgr.log(msg, verbose)
+    def log_tasks(self): return self.log_mgr.log_tasks()
 
     def rm_do_later(self, tsk):
         self.pause.remove_task(tsk)
