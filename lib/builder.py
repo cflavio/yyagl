c2984bf73a64a5dd2a0bfe694dc130e32cc4ed4b
Flavio Calva <flavio@ya2.it>
2020-03-25 21:13:01 +0100

    comments about the dependency inversion principle

--- lib/builder.py
+++ lib/builder.py
@@ -2,6 +2,12 @@ from yyagl.lib.p3d.p3d import LibP3d
 
 
 class LibBuilder(object):
+    '''This classe builds the implementation of the library abstraction  (for
+    the Dependency Inversion Principle).'''
 
     @staticmethod
-    def build(): return LibP3d()
+    def build():
+        '''This method actually builds the library implementation.
+        Now it builds Panda3D's implementation layer, but it may be used as a
+        dispatcher (e.g. for new Panda3D versions).'''
+        return LibP3d()
